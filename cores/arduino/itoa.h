9dd87031bd7d308d4cf58ab41441b956f3250c1e
Sandeep Mistry <s.mistry@arduino.cc>
2016-04-07 10:15:42 -0400

    Support building with gcc 4.9.3 or later
    Port of https://github.com/arduino/Arduino/pull/4830

--- cores/arduino/itoa.h
+++ cores/arduino/itoa.h
@@ -26,7 +26,14 @@ extern "C"{
 
 extern char* itoa( int value, char *string, int radix ) ;
 extern char* ltoa( long value, char *string, int radix ) ;
+#if __GNUC__ > 4 || \
+    (__GNUC__ == 4 && (__GNUC_MINOR__ > 9 || \
+                       (__GNUC_MINOR__ == 9 && \
+                        __GNUC_PATCHLEVEL__ > 2)))
+extern char* utoa( unsigned value, char *string, int radix ) ;
+#else
 extern char* utoa( unsigned long value, char *string, int radix ) ;
+#endif
 extern char* ultoa( unsigned long value, char *string, int radix ) ;
 
 #ifdef __cplusplus
