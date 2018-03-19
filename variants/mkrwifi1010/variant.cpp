b134f532a5c936600478c31b9162cd7f46cd97d1
Martino Facchin <m.facchin@arduino.cc>
2018-03-19 15:04:22 +0100

    WIP: add SPI interface

--- variants/mkrwifi1010/variant.cpp
+++ variants/mkrwifi1010/variant.cpp
@@ -159,6 +159,10 @@ const PinDescription g_APinDescription[] = {
   { PORTA,  1, PIO_DIGITAL,    (PIN_ATTR_NONE                                ), No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_NONE },
 
   { PORTA, 28, PIO_DIGITAL,    (PIN_ATTR_NONE                                ), No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_NONE },
+
+  { PORTA, 12, PIO_SERCOM,      (PIN_ATTR_NONE                               ), No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_NONE }, // MOSI: SERCOM4/PAD[0]
+  { PORTA, 13, PIO_SERCOM,      (PIN_ATTR_NONE                               ), No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_NONE }, // SCK:  SERCOM4/PAD[1]
+  { PORTA, 15, PIO_SERCOM,      (PIN_ATTR_NONE                               ), No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_NONE }, // MISO: SERCOM4/PAD[3]
 };
 
 const void* g_apTCInstances[TCC_INST_NUM + TC_INST_NUM]={ TCC0, TCC1, TCC2, TC3, TC4, TC5 };
@@ -226,13 +230,15 @@ void initVariant() {
   disable_battery_fet(!batteryPresent);
 #endif
 
-  // put GSM modem in reset on start to conserve power if it's not used
-  pinMode(NINA_RESETN, OUTPUT);
-  digitalWrite(NINA_RESETN, HIGH);
-
-  pinMode(NINA_ACK, INPUT_PULLUP);
   pinMode(NINA_GPIO0, OUTPUT);
   digitalWrite(NINA_GPIO0, HIGH);
+  delay(1);
+  pinMode(NINA_RESETN, OUTPUT);
+  digitalWrite(NINA_RESETN, HIGH);
+  delay(1);
+  digitalWrite(NINA_RESETN, LOW);
+
+  pinMode(NINA_ACK, INPUT_PULLUP);
 }
 
 // Serial1
